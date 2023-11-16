399cde80a442ad33c0fd044b63f2c36946e1d26d
SinghALGO <karanssandhu92@gmail.com>
2023-11-15 16:43:54 -0800

    Add function to return tail of the array

---
+++ tail.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const tail = function(array) {
+  if (array.length <= 1) {
+    return [];
+  }
+  return array.slice(1);
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
