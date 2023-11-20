66b0173da50a9fd4eaedfb20319d12ee9e4dcd13
SinghALGO <karanssandhu92@gmail.com>
2023-11-20 00:50:09 -0800

    Added function to count items in array and return an object

---
+++ countOnly.js
@@ -0,0 +1,14 @@
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+
+  for (let i of allItems) {
+    if (itemsToCount[i]) {
+      if (results[i]) {
+        results[i] += 1;
+      } else {
+        results[i] = 1;
+      }
+    }
+  }
+  return results;
+};
