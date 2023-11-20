fd7bd480ecc9ab5a62fc2c166a7558cde828ebc4
SinghALGO <karanssandhu92@gmail.com>
2023-11-20 14:30:44 -0800

    Added findKey function

---
+++ findKey.js
@@ -0,0 +1,10 @@
+const findKey = (object, callback) => {
+  let arr = Object.keys(object);
+  for (let i of arr) {
+    let res = callback(object[i]);
+    if (res === true) {
+      return i;
+    }
+  }
+  return;
+};
