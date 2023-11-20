ae561170791b9facef2db0437d12dcab4f22864f
SinghALGO <karanssandhu92@gmail.com>
2023-11-20 13:58:18 -0800

    Added map function

---
+++ map.js
@@ -0,0 +1,8 @@
+const map = function (array, callback = (word) => word[0]) {
+  const results = [];
+  for (let item of array) {
+    //push return value of the callback function
+    results.push(callback(item));
+  }
+  return results;
+};
