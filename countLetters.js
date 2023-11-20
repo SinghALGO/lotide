f52f298044ebe098139c52ee23151f134b9feb6b
SinghALGO <karanssandhu92@gmail.com>
2023-11-20 00:56:18 -0800

    Added function to count letters

---
+++ countLetters.js
@@ -0,0 +1,12 @@
+const countLetters = function (string) {
+  let result = {};
+  //Removing spaces between words in the string using .split(" ").join("")
+  for (let i of string.split(" ").join("")) {
+    if (result[i]) {
+      result[i] += 1;
+    } else {
+      result[i] = 1;
+    }
+  }
+  return result;
+};
