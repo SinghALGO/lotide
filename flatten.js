bcaf920727959f9c7dccd6573c5dffa627a6e4b9
SinghALGO <karanssandhu92@gmail.com>
2023-11-19 23:25:29 -0800

    Added function to flatten an array

---
+++ flatten.js
@@ -0,0 +1,14 @@
+//Function takes in an array and returns an array
+const flatten = function (arr) {
+  let array = [];
+  for (let i of arr) {
+    //case when the element is an array
+    if (Array.isArray(i)) {
+      //spread syntax used on nested array
+      array.push(...i);
+    } else {
+      array.push(i);
+    }
+  }
+  return array;
+};
