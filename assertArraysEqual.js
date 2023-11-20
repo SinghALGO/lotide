210688d9675c9c211e27bfbe5c11b77b36793c80
SinghALGO <karanssandhu92@gmail.com>
2023-11-19 22:55:29 -0800

    Added function to remove elements from array and return a new array

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -10,8 +10,7 @@ const eqArrays = function (arr1, arr2) {
   return true;
 };
 const assertArraysEqual = function (arr1, arr2) {
-  let res = eqArrays(arr1, arr2);
-  if (res) {
+  if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
