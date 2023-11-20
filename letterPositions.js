51c474de49931986f9d68f9ea3fb4f96bc25ea56
SinghALGO <karanssandhu92@gmail.com>
2023-11-20 01:05:11 -0800

    Added function to find letter position in a string

---
+++ letterPositions.js
@@ -0,0 +1,15 @@
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i in sentence) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        //push index of the element to the value array
+        results[sentence[i]].push(Number(i));
+      } else {
+        //add an array with index of the element as the value for key
+        results[sentence[i]] = [Number(i)];
+      }
+    }
+  }
+  return results;
+};
