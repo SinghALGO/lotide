6381ce7e72b78c3945202ab16590a86b9fcbcf0c
SinghALGO <karanssandhu92@gmail.com>
2023-11-19 22:33:27 -0800

    Added function to compare two arrays and print result to the console

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i in arr1) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function (arr1, arr2) {
+  let res = eqArrays(arr1, arr2);
+  if (res) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
