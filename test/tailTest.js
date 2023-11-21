c828508f8bada91cbae8056ab0c08de3867fa9ad
SinghALGO <karanssandhu92@gmail.com>
2023-11-20 19:55:57 -0800

    Added chai test for middle and tail

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,13 @@
+const assert = require("chai").assert;
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+describe("#Tail", () => {
+  it("returns [2,3] for [1,2,3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns ['Lighthouse','Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), [
+      "Lighthouse",
+      "Labs",
+    ]);
+  });
+});
