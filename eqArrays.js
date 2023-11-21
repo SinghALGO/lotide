a715b33ad73ccdf0e0b0ef92cf771cd6e494a44c
SinghALGO <karanssandhu92@gmail.com>
2023-11-20 19:05:28 -0800

    Refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // Function that takes two arrays, deep comapres them and returns a boolean
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -26,5 +18,4 @@ const eqArrays = function (arr1, arr2) {
     return true;
   }
 };
-
-assertEqual(eqArrays([1, 2], [1, 2, 3]), true);
+module.exports = eqArrays;
