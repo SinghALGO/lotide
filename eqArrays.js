4d2bd4698a402f24d8edd7b62514db222c25d374
SinghALGO <karanssandhu92@gmail.com>
2023-11-20 17:33:27 -0800

    Updated to include nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,20 +1,30 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-const eqArrays = function(arr1, arr2) {
+
+// Function that takes two arrays, deep comapres them and returns a boolean
+const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
-  }
-  for (let i in arr1) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+  } else {
+    for (let i in arr1) {
+      //case when the element is an array
+      if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+        //calling the function itself with the nested arrays as arguemnt
+        let res = eqArrays(arr1[i], arr2[i]);
+        if (!res) {
+          return false;
+        }
+      } else if (arr1[i] !== arr2[i]) {
+        return false;
+      }
     }
+    return true;
   }
-  return true;
 };
 
 assertEqual(eqArrays([1, 2], [1, 2, 3]), true);
