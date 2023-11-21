ff4bb08863c4a1fa5c57a6d311dcf7622c58968f
SinghALGO <karanssandhu92@gmail.com>
2023-11-20 20:05:27 -0800

    Aded index file to export multiple functions as an object

---
+++ index.js
@@ -0,0 +1,36 @@
+const assertArraysEqual = require("./assertArraysEqual");
+const assertEqual = require("./assertEqual");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const eqObjects = require("./eqObjects");
+const eqArrays = require("./eqArrays");
+const findKeyByValue = require("./findKeyByValue");
+const findKey = require("./findKey");
+const flatten = require("./flatten");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+module.exports = {
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqObjects,
+  eqArrays,
+  findKeyByValue,
+  findKey,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
+  head,
+  tail,
+  middle,
+};
