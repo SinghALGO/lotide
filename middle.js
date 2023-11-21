e92e62c776c455dcb33a637dc703c4e64c668ef9
SinghALGO <karanssandhu92@gmail.com>
2023-11-20 19:30:28 -0800

    Refactored middle

--- middle.js
+++ middle.js
@@ -1,21 +1,3 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i in arr1) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
 const middle = function (arr) {
   // case when only two elements
   if (arr.length < 3) {
@@ -29,3 +11,4 @@ const middle = function (arr) {
     return ans;
   }
 };
+module.exports = middle;
